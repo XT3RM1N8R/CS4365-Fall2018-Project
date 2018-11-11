c065ced3882ea167940bfce09f600344d972b228
Darien Sokolov <darien.sokolov@yahoo.com>
2018-11-11 03:42:12 -0600

    Added test for CreateUnit

--- Assets/SE II Project/Scripts/Instantiator.cs
+++ Assets/SE II Project/Scripts/Instantiator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Instantiator : MonoBehaviour { // Delete this class after testing is over
   
 	void Start () {
+    Unit.CreateUnit(Unit.FactionTypes.ALLIES, Unit.JobTypes.MAGE);
     Weapon.CreateWeapon(Weapon.WeaponTypes.BOW, Weapon.WeaponTiers.ADAMANTINE, transform);
   }
 }
