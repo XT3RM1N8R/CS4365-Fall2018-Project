b16d4d1419d85c093623864c9cdb22056be650ec
Darien Sokolov <darien.sokolov@yahoo.com>
2018-11-11 03:20:42 -0600

    Added test for Weapon runtime instantiation

---
+++ Assets/SE II Project/Scripts/Instantiator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour { // Delete this class after testing is over
+  
+	void Start () {
+    Weapon.CreateWeapon(Weapon.WeaponTypes.BOW, Weapon.WeaponTiers.ADAMANTINE, transform);
+  }
+}
